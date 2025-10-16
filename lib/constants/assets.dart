428ac9cdd8d0be54af3a5e3cf509764addaa0253
Muhammad Azri <muhammad.azri.f.s@gmail.com>
2025-10-16 15:20:47 +0700

    refactor(assets): reorganize and standardize asset structure
    - Create organized directory structure (icons/navigation, icons/buttons, icons/ui, icons/shop, images/banners, images/photos)
    - Rename all assets to snake_case following Flutter conventions
    - Remove corrupted file and consolidate duplicate icons
    - Add asset paths to pubspec.yaml
    - Create Assets constants class for type-safe asset references

---
+++ lib/constants/assets.dart
@@ -0,0 +1,50 @@
+/// Asset paths for the application
+/// This file contains all asset paths used throughout the app
+class Assets {
+  Assets._();
+
+  // Navigation Icons
+  static const String navHome = 'assets/icons/navigation/home.png';
+  static const String navHomeActive = 'assets/icons/navigation/home_active.png';
+  static const String navDiscover = 'assets/icons/navigation/discover.png';
+  static const String navMall = 'assets/icons/navigation/mall.png';
+  static const String navMallActive = 'assets/icons/navigation/mall_active.png';
+  static const String navInbox = 'assets/icons/navigation/inbox.png';
+  static const String navAccount = 'assets/icons/navigation/account.png';
+
+  // Button Icons
+  static const String btnPosts = 'assets/icons/buttons/posts.png';
+  static const String btnServices = 'assets/icons/buttons/services.png';
+  static const String btnShop = 'assets/icons/buttons/shop.png';
+  static const String btnIcon1 = 'assets/icons/buttons/icon_1.png';
+  static const String btnIcon2 = 'assets/icons/buttons/icon_2.png';
+  static const String btnIcon3 = 'assets/icons/buttons/icon_3.png';
+  static const String btnIcon4 = 'assets/icons/buttons/icon_4.png';
+  static const String btnIcon5 = 'assets/icons/buttons/icon_5.png';
+
+  // UI Icons
+  static const String iconArrow = 'assets/icons/ui/arrow.png';
+  static const String iconArrowAlt = 'assets/icons/ui/arrow_alt.png';
+  static const String iconCalendar = 'assets/icons/ui/calendar.png';
+  static const String iconClock = 'assets/icons/ui/clock.png';
+  static const String iconClockAlt = 'assets/icons/ui/clock_alt.png';
+  static const String iconLocation = 'assets/icons/ui/location.png';
+  static const String iconLocationAlt = 'assets/icons/ui/location_alt.png';
+  static const String iconFilter = 'assets/icons/ui/filter.png';
+  static const String iconSearch = 'assets/icons/ui/search.png';
+
+  // Shop Icons
+  static const String shopPlantsMain = 'assets/icons/shop/plants_main.png';
+  static const String shopPlants1 = 'assets/icons/shop/plants_1.png';
+  static const String shopPlants2 = 'assets/icons/shop/plants_2.png';
+  static const String shopPlants3 = 'assets/icons/shop/plants_3.png';
+  static const String shopPlants4 = 'assets/icons/shop/plants_4.png';
+  static const String shopPlants5 = 'assets/icons/shop/plants_5.png';
+
+  // Banner Images
+  static const String bannerHome = 'assets/images/banners/home_banner.jpg';
+  static const String bannerTrendingDiscoveries = 'assets/images/banners/trending_discoveries.jpg';
+
+  // Photos
+  static const String photoImage = 'assets/images/photos/image.jpg';
+}
